6c807765efeb894ec9172b4c70957acb4a292447
Alejandro-cj <aloflow_345@hotmail.com>
2024-08-29 09:58:33 -0500

    index.ts

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import connectDB from './config/db';
+import bookRoutes from './routes/bookRoutes';
+
+const app = express();
+
+// Conectar a la base de datos
+connectDB();
+
+// Middlewares
+app.use(express.json());
+
+// Rutas
+app.use('/books', bookRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
